3cd9a0d2ba81a737c7809aec3cbc465ce9a999d8
nanumhk <nanumhk@gmail.com>
2021-01-26 21:23:10 -0500

    Added console writeline

---
+++ New folder/ArrayLiat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayLiat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          ArrayList myValue = new ArrayList();
+            myValue.Add(10);
+            myValue.Add("this is a pen");
+            myValue.Add(10.23);
+            foreach (var value in myValue)
+            {
+                Console.WriteLine(value);
+
+            }
+            {
+                Console.WriteLine( "a");
+            }
+
+        }
+    }
+}
